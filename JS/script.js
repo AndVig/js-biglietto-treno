fe6bfdf6ae91d710adba51df749b6762b251985d
AndVig <andrea.vigna2@gmail.com>
2024-04-26 18:00:07 +0200

    problem solved

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,9 @@
-let lenght= prompt("Quanti km vuoi percorrere?");
-lenght=parseInt(lenght);
-console.log("lenght: ",lenght);
-
+let length= prompt("Quanti km vuoi percorrere?");
+length=parseInt(length);
+console.log("length: ",length);
 let age=prompt("Quanti anni hai?");
 age=parseInt(age);
 console.log("age: ",age);
-
 //var priceOverKm = 0.21;
 var fullPrice = length * 0.21 ;
 var line1= "Il prezzo del biglietto è: ";
