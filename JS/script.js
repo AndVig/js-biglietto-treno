349951af8093179a9b77a9cae695580c41309d01
AndVig <andrea.vigna2@gmail.com>
2024-04-26 17:53:33 +0200

    add script (but doesn't work, dunno why)

---
+++ JS/script.js
@@ -0,0 +1,25 @@
+let lenght= prompt("Quanti km vuoi percorrere?");
+lenght=parseInt(lenght);
+console.log("lenght: ",lenght);
+
+let age=prompt("Quanti anni hai?");
+age=parseInt(age);
+console.log("age: ",age);
+
+//var priceOverKm = 0.21;
+var fullPrice = length * 0.21 ;
+var line1= "Il prezzo del biglietto è: ";
+var value=" euro";
+console.log(fullPrice);
+
+
+if(age <= 18){
+    document.getElementById('cost').innerHTML= line1 + ((fullPrice / 100) * 80) + value;
+}
+else if(age>=65){
+    document.getElementById('cost').innerHTML= line1+ ((fullPrice/100)*60)+ value;
+}
+else{
+    document.getElementById('cost').innerHTML= line1+ fullPrice+ value;
+
+}
